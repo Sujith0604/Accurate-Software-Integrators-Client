f41a1ec746bd11ec8eb459e2523c7c8946553bf6
Sujith Karthikaiselvan <sujithkarthikaiselvan@gmail.com>
2025-01-05 17:52:15 +0530

    model

--- src/Components/ModalComponent.jsx
+++ src/Components/ModalComponent.jsx
@@ -10,7 +10,7 @@ const ModalComponent = ({
 }) => {
   return (
     <div className=" fixed inset-0 overflow-auto md:overflow-hidden   bg-black bg-opacity-55 backdrop-blur-sm flex items-center justify-center z-50 h-screen w-full ">
-      <div className=" text-white bg-[url('/images/BackgroundModal.png')] bg-cover rounded-lg shadow-md flex flex-col gap-5 h-fit w-[300px] md:w-[600px] ">
+      <div className=" p-4 text-white bg-[url('/images/BackgroundModal.png')] bg-cover rounded-lg shadow-md flex flex-col gap-5 h-fit w-[350px] md:w-[600px] lg:h-[600px] lg:w-[600px] ">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">{title}</h1>
           <button
